03ccb0054b12c07348dec1dab1e7f5ee8867a091
Alexandre Gherschon <alexandre.gherschon@gmail.com>
2019-03-17 07:24:41 +0200

    WIP use of sqldelight

--- core/src/main/kotlin/com/analytics/core/helper/EventsManager.kt
+++ core/src/main/kotlin/com/analytics/core/helper/EventsManager.kt
@@ -1,9 +1,14 @@
 package com.analytics.core.helper
 
+import android.app.Application
 import com.analytics.core.model.Event
 import com.analytics.core.network.api.EventsAPI
+import com.squareup.sqldelight.android.AndroidSqliteDriver
+import com.squareup.sqldelight.db.SqlDriver
 
 actual object EventsManager {
+
+    lateinit var application: Application
 
     actual fun manage(event: Event) {
 
@@ -11,5 +16,8 @@ actual object EventsManager {
         EventsAPI.postEvent(AnalyticsConfig.getBaseUrl(), event) {
             println("sent the event, success = ${it.success}")
         }
+
+        // database driver
+        val driver: SqlDriver = AndroidSqliteDriver(AnalyticsDatabase.Schema, application, "test.db")
     }
 }
